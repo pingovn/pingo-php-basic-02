b7d3ece585ffd7419d3d99426a71c17b03198930
ITbaby05 <itbaby05@yahoo.com.vn>
2015-03-13 13:16:07 +0700

    Update slides and exam

--- solutions/exercise4.php
+++ solutions/exercise4.php
@@ -1,13 +1,13 @@
 <html>
 	<form action="<?php echo $_SERVER['PHP_SELF']; ?>" method="POST">
 	    <label style="font-weight:bold">Email: </label>
-        <input type="text" name="year" value="<?php if (isset($_POST['year'])) echo $_POST['year'] ?>">
+        <input type="text" name="email" value="<?php if (isset($_POST['year'])) echo $_POST['year'] ?>">
         <input type="submit" name="submit" value="Kiem tra"><br>		
     </form>
 <?php
 // check email
-$regex = '/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_-]+.[a-zA-Z0-9_-]+$/'; 
-if (empty($_POST['year']) || !preg_match($regex, $_POST['year'])) {
+$regex = '/^[a-zA-Z0-9_.-]+@[a-zA-Z0-9_-]+(.[a-zA-Z0-9_-]){0,1}.[a-zA-Z0-9_-]+$/'; 
+if (empty($_POST['email']) || !preg_match($regex, $_POST['email'])) {
 	if (isset($_POST['submit']))
 		echo '<label style="color:red">Email cua ban khong hop le</label>';
 }
